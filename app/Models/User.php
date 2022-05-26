8ca89d6f2feaf6f989d8393a8520485c0c5ac132
Bruno Holanda <br.holanda15@gmail.com>
2022-05-26 19:24:39 -0300

    Creating Model Comment and his relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
             }
         })->get();
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'user_id', 'id');
+    }
 }
