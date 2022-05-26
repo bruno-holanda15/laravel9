cbb8e221b4b986c3489f5d84da27dee6b253ae82
Bruno Holanda <br.holanda15@gmail.com>
2022-05-26 19:09:37 -0300

    Changing style

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,7 +8,7 @@
     </h1>
     <form action="{{ route('users.index') }} " class="py-2 px-3" method="GET">
         <input type="text" name="search" placeholder="Search">
-        <button class="font-mono shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+        <button class="font-mono shadow bg-purple-400 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
             Search garotinhoo
         </button>
         <button  class="bg-green-500 rounded py-2 px-3">
@@ -19,22 +19,22 @@
         <thead>
             <tr>
               <th
-                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-400 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
               >
                 Nome
               </th>
               <th
-                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-400 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
               >
                 E-mail
               </th>
               <th
-                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-400 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
               >
                 Editar
               </th>
               <th
-                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-400 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
               >
                 Detalhes
               </th>
